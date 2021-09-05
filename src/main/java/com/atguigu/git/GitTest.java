e8470583d30ad93681960004329476330a7d62e4
tom_glb <goodMorning_glb@atguigu.com>
2021-09-05 17:36:10 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author stf
+ * @create 2021/9/5
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
